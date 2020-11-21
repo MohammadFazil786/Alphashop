f76b2a77bd4260c1d7b2bab72fe212cf180f7578
Fazil <fazilmohammad258@gmail.com>
2020-11-21 20:52:10 +0530

    Updates

---
+++ precache-manifest.fd90ad8eb6dc42201303e665236eaee2.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb6011793c3c25fae78d491a85e6c5f6",
+    "url": "/Alphashop/index.html"
+  },
+  {
+    "revision": "78b018349230eb3d873d",
+    "url": "/Alphashop/static/css/2.b9f3e128.chunk.css"
+  },
+  {
+    "revision": "d75ccc15072716154fc6",
+    "url": "/Alphashop/static/css/main.bf894906.chunk.css"
+  },
+  {
+    "revision": "78b018349230eb3d873d",
+    "url": "/Alphashop/static/js/2.5d291e87.chunk.js"
+  },
+  {
+    "revision": "f59da382ef0e40e6fd7e14f0c7baae9f",
+    "url": "/Alphashop/static/js/2.5d291e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75ccc15072716154fc6",
+    "url": "/Alphashop/static/js/main.65cd1a95.chunk.js"
+  },
+  {
+    "revision": "49ea73c07b9e195afd44",
+    "url": "/Alphashop/static/js/runtime-main.2d8ef566.js"
+  },
+  {
+    "revision": "bf36ab447852b1918e8b8aaa6f7f97a5",
+    "url": "/Alphashop/static/media/amz1.bf36ab44.jpg"
+  },
+  {
+    "revision": "9bb138760a5f6f39a74f1a58c9c65772",
+    "url": "/Alphashop/static/media/amz3.9bb13876.jpg"
+  },
+  {
+    "revision": "b76530133bb4ca7c09f48d89ac245262",
+    "url": "/Alphashop/static/media/amz4.b7653013.jpg"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/Alphashop/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/Alphashop/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "e616633e6c2de6798af4dbe033db6350",
+    "url": "/Alphashop/static/media/awek1.e616633e.jpg"
+  },
+  {
+    "revision": "edb72fc492b8a2f5ba02d2f13abae982",
+    "url": "/Alphashop/static/media/detailsImage-1.edb72fc4.jpg"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/Alphashop/static/media/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/Alphashop/static/media/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/Alphashop/static/media/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/Alphashop/static/media/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/Alphashop/static/media/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/Alphashop/static/media/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/Alphashop/static/media/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/Alphashop/static/media/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/Alphashop/static/media/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/Alphashop/static/media/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/Alphashop/static/media/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/Alphashop/static/media/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/Alphashop/static/media/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/Alphashop/static/media/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/Alphashop/static/media/fa-solid-900.8e1ed89b.woff2"
+  },
+  {
+    "revision": "7ded5f97eee187d26005f2b61ef21b13",
+    "url": "/Alphashop/static/media/fazil1.7ded5f97.jpg"
+  },
+  {
+    "revision": "7783142650d95af57f968d3684213e6c",
+    "url": "/Alphashop/static/media/pic3.77831426.jpeg"
+  },
+  {
+    "revision": "798fca36c3aa7098477c6acee132bc4a",
+    "url": "/Alphashop/static/media/rahul1.798fca36.jpg"
+  }
+]);
